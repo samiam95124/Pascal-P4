9a679fc7e2c5ecea9bbd23311c349ed7b8320830
samiam95124 <samiam@moorecad.com>
2015-12-17 09:20:53 -0800

    Copied and adapted from p5.

--- gpc/p4.bat
+++ gpc/p4.bat
@@ -4,15 +4,21 @@ rem Compile with p4 using GPC
 rem
 rem Execute with:
 rem
-rem p4 <file>
+rem p4 <sourcefile> [<inputfile>[<outputfile>]]
 rem
-rem where <file> is the name of the source file without
+rem where <sourcefile> is the name of the source file without
 rem extention. The Pascal file is compiled and run.
 rem Any compiler errors are output to the screen. Input
 rem and output to and from the running program are from
 rem the console, but output to the prr file is placed
-rem in <file>.out.
+rem in <sourcefile>.out.
+rem
 rem The intermediate code is placed in <file>.p4.
+rem
+rem If <inputfile> and <outputfile> are specified, then these will be
+rem placed as input to the "prd" file, and output from the "prr" file.
+rem Note that the prd file cannot or should not be reset, since that
+rem would cause it to back up to the start of the intermediate code.
 rem
 if not "%1"=="" goto paramok
 echo *** Error: Missing parameter
@@ -22,13 +28,27 @@ if exist "%1.pas" goto fileexists
 echo *** Error: Missing %1.pas file
 goto stop
 :fileexists
+if "%2"=="" goto continue
+if exist "%2" goto continue
+echo *** Error: Missing %2 input file
+goto stop
+:continue
 echo.
 echo Compiling and running %1
 echo.
-pcom < %1.pas
-del %1.p4 > temp
-copy prr %1.p4 > temp
-del prr
-copy %1.p4 prd > temp
+cp %1.pas prd
+pcom
+mv prr %1.p4
+if not "%2"=="" goto useinputfile
+cp %1.p4 prd
+goto run
+:useinputfile
+rem The input file, if it exists, gets put on the end of the intermediate
+cat %1.p4 %2 > prd
+:run
 pint
-del temp
+if "%3"=="" goto stop
+cp %prr %3
+:stop
+rm -f prd
+rm -f prr
