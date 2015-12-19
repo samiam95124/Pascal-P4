c1beffd0f739ff602e356fedbcdcc341e42e7b52
samiam95124 <samiam@moorecad.com>
2015-12-18 17:28:33 -0800

    Converted regress scripts from p5 to p4.

--- bin/regress.bat
+++ bin/regress.bat
@@ -7,28 +7,8 @@ rem to a "gold" standard file
 rem
 call testprog sample_programs\hello
 call testprog sample_programs\roman
-call testprog sample_programs\match
-call testprog sample_programs\startrek
-call testprog sample_programs\basics
-call testprog sample_programs\drystone
-call testprog sample_programs\fbench
-call testprog sample_programs\prime
 call testprog sample_programs\qsort
 rem
-rem Now run the ISO7185pat compliance test
+rem Now run the standard compliance test
 rem
-call testprog standard_tests\iso7185pat
-rem
-rem Run previous versions of the system and Pascal-S
-rem
-call testpascals
-call testp2
-call testp4
-rem
-rem Run pcom self compile (note this runs on P5 only)
-rem
-call cpcoms
-rem
-rem Run pint self compile (note this runs on P5 only)
-rem
-call cpints
+call testprog standard_tests\standardp
